1b07a33f9857dc5f2c0ff8a6e2aa88c804f571b3
Nickolas lira7 <125027693+Nickolaslira7@users.noreply.github.com>
2024-04-29 09:05:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     database: 'aulaback',
     password: 'ds564',
     port: 5432,
-    });
+});
 
 const app = express();
 const PORT = 3000;
@@ -15,8 +15,8 @@ const PORT = 3000;
 app.use(express.json(''));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-    });
+    res.send('Olá Mundo');
+});
 
 app.get('/Usuarios', async (req, res) => {
     try {
@@ -25,24 +25,24 @@ app.get('/Usuarios', async (req, res) => {
             total: rows.length,
             Usuarios: rows,
         });
-        }
+    }
     catch (error) {
-        console.log("error in get Usuarios", error);
+        console.log("erro ao obter Usuarios", error);
         res.status(500).json(error);
-        }
-    });
+    }
+});
 
 app.get('/Usuarios/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const { rows } = await pool.query('SELECT * FROM Usuarios WHERE id = $1', [id]);
         res.json(rows);
-        }
+    }
     catch (error) {
-        console.log("error in get Usuarios id",error);
+        console.log("erro ao obter Usuario por id",error);
         res.status(500).json(error);
-        }
-    });
+    }
+});
 
 app.post('/Usuarios', async (req, res) => {
     const { nome, sobrenome, email, datanascimento} = req.body;
@@ -52,99 +52,98 @@ app.post('/Usuarios', async (req, res) => {
         const { rows } = await pool.query(
             'INSERT INTO Usuarios (nome, sobrenome, email, datanascimento, idade, signo) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
             [nome, sobrenome, email, datanascimento, idade, signo]
-            );
+        );
         res.json(
             {
-                message: 'User created successfully',
-                user: rows[0],
+                message: 'Usuário criado com sucesso',
+                usuario: rows[0],
             }
         );
-        }
-    catch (error) {
-        console.log("error in post Usuarios",error);
-        res.status(500).json(error);
-        }
     }
-    );
+    catch (error) {
+        console.log("erro ao criar Usuario",error);
+        res.status(500).json(error);
+    }
+});
 
 app.put('/Usuarios/:id', async (req, res) => {
     const { id } = req.params;
     const { nome, sobrenome, email, datanascimento } = req.body;
-    const idade = calculateAge(new Date(datanascimento));
-    const zodiac_sign = calculateZodiacSign(new Date(datanascimento));
+    const idade = calcularIdade(new Date(datanascimento));
+    const signo = CalcularSigno(new Date(datanascimento));
     try {
         const { rows } = await pool.query(
-            'UPDATE Usuarios SET nome = $1, sobrenome = $2, email = $3, datanascimento = $4, idade = $5, zodiac_sign = $6 WHERE id = $7 RETURNING *',
-            [nome, sobrenome, email, datanascimento, idade, zodiac_sign, id]
-            );
-        res.json(
-            rows.length > 0 ? {message: 'Update sucess!', rows}: { message: 'User not found' }
+            'UPDATE Usuarios SET nome = $1, sobrenome = $2, email = $3, datanascimento = $4, idade = $5, signo = $6 WHERE id = $7 RETURNING *',
+            [nome, sobrenome, email, datanascimento, idade, signo, id]
         );
-        }
+        res.json(
+            rows.length > 0 ? {message: 'Atualização bem-sucedida!', rows}: { message: 'Usuário não encontrado' }
+        );
+    }
     catch (error) {
-        console.log("error in put Usuarios",error);
+        console.log("erro ao atualizar Usuario",error);
         res.status(500).json(error);
-        }
-    });
+    }
+});
 
 app.delete('/Usuarios/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const { rows } = await pool.query('DELETE FROM Usuarios WHERE id = $1 RETURNING *', [id]);
-            res.json({
-                message: rows.length > 0 ? 'User deleted successfully' : 'User not found',
-                user: rows[0],
-            });
-        }
+        res.json({
+            message: rows.length > 0 ? 'Usuário excluído com sucesso' : 'Usuário não encontrado',
+            usuario: rows[0],
+        });
+    }
     catch (error) {
-        console.log("error in delete Usuarios",error);
+        console.log("erro ao excluir Usuario",error);
         res.status(500).json(error);
-        }
-    });
+    }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
+    console.log(`O servidor está rodando na porta ${PORT}`);
+});
 
 
 const calcularIdade = (datanascimento) => {
-    const today = new Date();
-    const birthDate = new Date(datanascimento);
-    let idade = today.getFullYear() - birthDate.getFullYear();
-    const month = today.getMonth() - birthDate.getMonth();
-    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+    const hoje = new Date();
+    const dataNascimento = new Date(datanascimento);
+    let idade = hoje.getFullYear() - dataNascimento.getFullYear();
+    const mes = hoje.getMonth() - dataNascimento.getMonth();
+    if (mes < 0 || (mes === 0 && hoje.getDate() < dataNascimento.getDate())) {
         idade--;
-        }
+    }
     return idade;
-    };
+};
 
-    const CalcularSigno = (datanascimento) => {
-        const mes = datanascimento.getMonth() + 1;
-        const dia = datanascimento.getDate();
-    
-        if ((mes === 3 && dia >= 21) || (mes === 4 && dia <= 19)) {
-            return 'Aries';
-        } else if ((mes === 4 && dia >= 20) || (mes === 5 && dia <= 20)) {
-            return 'Touros';
-        } else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 20)) {
-            return 'Gemeos';
-        } else if ((mes === 6 && dia >= 21) || (mes === 7 && dia <= 22)) {
-            return 'Cancer';
-        } else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) {
-            return 'Leão';
-        } else if ((mes === 8 && dia >= 23) || (mes === 9 && dia <= 22)) {
-            return 'Virgem';
-        } else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 22)) {
-            return 'Libra';
-        } else if ((mes === 10 && dia >= 23) || (mes === 11 && dia <= 21)) {
-            return 'escorpião';
-        } else if ((mes === 11 && dia >= 22) || (mes === 12 && dia <= 21)) {
-            return 'Sagitario';
-        } else if ((mes === 12 && dia >= 22) || (mes === 1 && dia <= 19)) {
-            return 'Capricornio';
-        } else if ((mes === 1 && dia >= 20) || (mes === 2 && dia <= 18)) {
-            return 'Aquario';
-        } else {
-            return 'Pisces';
-        }
-    };
+const CalcularSigno = (datanascimento) => {
+    const mes = datanascimento.getMonth() + 1;
+    const dia = datanascimento.getDate();
+
+    if ((mes === 3 && dia >= 21) || (mes === 4 && dia <= 19)) {
+        return 'Áries';
+    } else if ((mes === 4 && dia >= 20) || (mes === 5 && dia <= 20)) {
+        return 'Touro';
+    } else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 20)) {
+        return 'Gêmeos';
+    } else if ((mes === 6 && dia >= 21) || (mes === 7 && dia <= 22)) {
+        return 'Câncer';
+    } else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) {
+        return 'Leão';
+    } else if ((mes === 8 && dia >= 23) || (mes === 9 && dia <= 22)) {
+        return 'Virgem';
+    } else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 22)) {
+        return 'Libra';
+    } else if ((mes === 10 && dia >= 23) || (mes === 11 && dia <= 21)) {
+        return 'Escorpião';
+    } else if ((mes === 11 && dia >= 22) || (mes === 12 && dia <= 21)) {
+        return 'Sagitário';
+    } else if ((mes === 12 && dia >= 22) || (mes === 1 && dia <= 19)) {
+        return 'Capricórnio';
+    } else if ((mes === 1 && dia >= 20) || (mes === 2 && dia <= 18)) {
+        return 'Aquário';
+    } else {
+        return 'Peixes';
+    }
+};
